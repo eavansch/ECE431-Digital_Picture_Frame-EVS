d89171d96462fde5436c020103a1774ef393e613
Erik Van Schijndel <89333755+eavansch@users.noreply.github.com>
2023-12-13 17:31:39 -0600

    Added project files

---
+++ ECE431-Microcontrollers Final - EVS/Debug/main.d
@@ -0,0 +1,102 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr6989.h
+main.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h
+main.obj: ../Grlib/grlib/grlib.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdint.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_stdint40.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_stdint.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdbool.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/assert.h
+main.obj: ../LcdDriver/lcd_driver.h
+main.obj: C:/Users/erikv/workspace_v10/finalFinal/Grlib/grlib/grlib.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdio.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdarg.h
+main.obj: ../rachel.c
+main.obj: ../Grlib/grlib/grlib.h
+main.obj: ../EA.c
+main.obj: ../Grlib/grlib/grlib.h
+main.obj: ../ENR.c
+main.obj: ../Grlib/grlib/grlib.h
+main.obj: ../erik.c
+main.obj: ../Grlib/grlib/grlib.h
+main.obj: ../erik2.c
+main.obj: ../Grlib/grlib/grlib.h
+
+../main.c:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr6989.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h:
+
+../Grlib/grlib/grlib.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdbool.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/assert.h:
+
+../LcdDriver/lcd_driver.h:
+
+C:/Users/erikv/workspace_v10/finalFinal/Grlib/grlib/grlib.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdio.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdarg.h:
+
+../rachel.c:
+
+../Grlib/grlib/grlib.h:
+
+../EA.c:
+
+../Grlib/grlib/grlib.h:
+
+../ENR.c:
+
+../Grlib/grlib/grlib.h:
+
+../erik.c:
+
+../Grlib/grlib/grlib.h:
+
+../erik2.c:
+
+../Grlib/grlib/grlib.h:
+
---
+++ ECE431-Microcontrollers Final - EVS/Debug/subdir_vars.mk
@@ -0,0 +1,59 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr6989.cmd 
+
+C_SRCS += \
+../EA.c \
+../ENR.c \
+../erik.c \
+../erik2.c \
+../main.c \
+../rachel.c 
+
+C_DEPS += \
+./EA.d \
+./ENR.d \
+./erik.d \
+./erik2.d \
+./main.d \
+./rachel.d 
+
+OBJS += \
+./EA.obj \
+./ENR.obj \
+./erik.obj \
+./erik2.obj \
+./main.obj \
+./rachel.obj 
+
+OBJS__QUOTED += \
+"EA.obj" \
+"ENR.obj" \
+"erik.obj" \
+"erik2.obj" \
+"main.obj" \
+"rachel.obj" 
+
+C_DEPS__QUOTED += \
+"EA.d" \
+"ENR.d" \
+"erik.d" \
+"erik2.d" \
+"main.d" \
+"rachel.d" 
+
+C_SRCS__QUOTED += \
+"../EA.c" \
+"../ENR.c" \
+"../erik.c" \
+"../erik2.c" \
+"../main.c" \
+"../rachel.c" 
+
+
